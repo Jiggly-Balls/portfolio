d06a7e4db54146cc3b8395654fb4fb008903339d
Jiggly Balls <jigglyballs9000@gmail.com>
2025-11-18 21:24:20 +0530

    Fixed repeated item switch bug

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const favouriteThings = ["games", "API wrappers", "libraries", "websites"];
 let currentIndex = favouriteThings.length - 1;
 
 function cycle() {
-  const element = favouriteThings[currentIndex];
   currentIndex = (currentIndex + 1) % favouriteThings.length;
+  const element = favouriteThings[currentIndex];
 
   document.getElementById("myThing").innerText = element;
 }
